f76dc49983f5cf23cf789d57cc96aa604a984807
DavideLopez <davidelopez7@gmail.com>
2022-09-14 17:52:18 +0200

    inserita navbar

---
+++ src/components/MainNav.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="nav">
+     <div class="container">
+      <div class="logo">
+        <img src="../assets/img/logo-dark.png" alt="logo" height="25px">
+      </div>
+
+      <div class="navmenu">
+        <ul class="navmenu">
+          <li>HOME</li>
+          <li>LANDING</li>
+          <li>PAGES</li>
+          <li>DOCS</li>
+        </ul>
+
+      </div>
+
+     <div class="buttons">
+      <font-awesome-icon icon="fa-magnifying-glass"></font-awesome-icon>
+
+      <div class="active">
+        <font-awesome-icon icon="fa-brands fa-github"></font-awesome-icon>
+      </div>
+      
+    <div class="active">
+      <font-awesome-icon icon="fa-brands fa-stack-overflow"></font-awesome-icon>
+    </div>
+
+    <div class="active">
+      <font-awesome-icon icon="fa-regular fa-user"></font-awesome-icon>
+    </div>
+     </div>
+     </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainNav',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  
+  .container{
+    max-width: 1400px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    padding: 20px;
+
+     .navmenu{
+      display: flex;
+      gap: 35px;
+      
+      
+     }
+
+     li:hover {
+      color: blue;
+      cursor: pointer;
+     }
+
+     .buttons {
+      color: black;
+      display: flex;
+      gap: 20px;
+
+      .active {
+        color: blue;
+        // background-color: aquamarine;
+      }
+     }
+
+  }
+
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+import { faGithub, faStackOverflow } from '@fortawesome/free-brands-svg-icons'
+
+
+/* add icons to the library */
+library.add(faMagnifyingGlass,faUser,faGithub,faStackOverflow)
+
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
