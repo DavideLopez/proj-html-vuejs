c97ce63b26e21462c4cf1c5f588822b3c9e253a1
DavideLopez <davidelopez7@gmail.com>
2022-09-15 18:03:20 +0200

    fix commenti e generata sezione 5

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -1,24 +1,26 @@
-<template>   <!--   QUI TUTTO IL CONTENUTO DELLE VARIE SEZIONI -->
+   <!--   TUTTO IL CONTENUTO DELLE VARIE SEZIONI -->
+<template>
     <div>
         <FirstSection />
         <SecondSection />
         <ThirdSection />
         <FourthSection />
-       <!--  <FifthSection /> -->
+        <FifthSection /> 
 
     </div>
 </template>
 
-<script>   //DA CREARE!!!!!!!!!!!!!!!!! FORSE SONO 5 SEZIONI
+<!--     QUI TUTTO L'IMPORT DELLE VARIE SEZIONI -->
+<script>    
 import FirstSection from './FirstSection.vue';
 import SecondSection from './SecondSection.vue';
 import ThirdSection from './ThirdSection.vue';
 import FourthSection from './FourthSection.vue';
-// import FifthSection from './FifthSection.vue';
+import FifthSection from './FifthSection.vue';
 
 export default {
     name: "TheMain",
-    components: { FirstSection, SecondSection, ThirdSection, FourthSection,},  //DA CREARE!!!!!!!!!!!!!!!!! FORSE SONO 5 SEZIONI
+    components: { FirstSection, SecondSection, ThirdSection, FourthSection, FifthSection,},  
 }
 
 </script>
